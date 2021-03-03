1554027c3a7023d384d261c1eb21481cd5fe7e25
Vadim <zhukov.vd.s@gmail.com>
2021-03-04 00:21:55 +0300

    simple example

--- HomeWork_6/index.js
+++ HomeWork_6/index.js
@@ -1,7 +1,27 @@
-const express = require('express')
+const httpServer = require('http').createServer(handler)
+const io = require('socket.io')(httpServer)
+const fs = require('fs')
+const path = require('path')
 
-const app = express()
+function handler(req, res) {
+  fs.readFile(path.join(__dirname, 'index.html'), 
+  function (err, data) {
+    if (err) {
+      res.writeHead(500)
+      return res.end('Error loading index.html')
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end(data)
+  })
+}
+
+io.on('connection', (socket) => {
+  socket.emit('news', { greeting: "Hello socket.io"})
+  socket.on('greeting', (data) => {
+    console.log(data)
+  })
+})
 
 const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
+httpServer.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
