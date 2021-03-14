a5669578d5e7c3db074af28e1e6812343feebc4d
Vadim <zhukov.vd.s@gmail.com>
2021-03-14 17:49:52 +0300

    Chto-to rabotaet

--- HomeWork_3/index.js
+++ HomeWork_3/index.js
@@ -1,21 +1,76 @@
-let str = "3 2 1"
-let str_arr = str.split(' ')
-str_arr.forEach((elem, i, a) => a[i] = Number.parseInt(elem))
-console.log(str_arr)
-str_arr.sort((a, b) => a - b)
-console.log(str_arr)
+const fs = require('fs')
+const util = require('util')
+const stream = require('stream')
+const path = require('path')
+const pipeline = util.promisify(stream.pipeline)
+const EventEmitter = require('events')
+
+const readStream1 = fs.createReadStream(path.join(__dirname, "/files/1.txt"), {encoding: 'utf-8'})
+const readStream2 = fs.createReadStream(path.join(__dirname, "/files/2.txt"), {encoding: 'utf-8'})
+const writeStream1 = fs.createWriteStream(path.join(__dirname, "/files/3.txt"))
+
+const myEmitter = new EventEmitter()
+
+let buff = []
+let continueProcess = true
+
+function writeChunkToTemporaryBuffer (chunk) {
+  let numbersArr = chunk.split('\n')
+  numbersArr.forEach((element, i, a) => a[i] = Number.parseInt(element))
+  buff = buff.concat(numbersArr)
+}
+
+readStream1.on('data', chunk => {
+  console.log(111);
+  writeChunkToTemporaryBuffer(chunk)
+  readStream1.pause()
+})
+
+
+readStream2.on('data', chunk => {
+  console.log(222);
+  writeChunkToTemporaryBuffer(chunk)
+  readStream2.pause()
+  myEmitter.emit('go')
+})
+
+
+readStream2.on('end', chunk => {
+  continueProcess = false
+  console.log(444);
+})
+
+
+myEmitter.on('go', () => {
+  console.log(buff)
+  buff.sort((a, b) => a - b)
+  console.log(buff)
+  for (let i = 0; i < buff.length; i++)
+      writeStream1.write(`${buff[i]}\n`)
+  buff = []
+})
+
+/*
+
+while(continueProcess) {
+  console.log(333);
+  readStream1.read()
+  buff.sort((a, b) => a - b)
+  for (let i = 0; i < buff.length; i++)
+      writeStream1.write(`${buff[i]}\n`)
+  buff = []
+  readStream1.resume()
+  readStream2.resume()
+}
+*/
 
 
 
 /*
-const fs = require('fs')
-const util = require('util')
-const stream = require('stream')
-const pipeline = util.promisify(stream.pipeline)
-
 async function run() {
     await pipeline(
         fs.createReadStream('lowercase.txt'),
+
         async function* (source) {
             for await (const chunk of source) {
                 yield String(chunk).toUpperCase()
