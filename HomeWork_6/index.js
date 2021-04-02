8ee72b43e619b8b52eca16d94cb652290e1f5b89
Vadim <zhukov.vd.s@gmail.com>
2021-04-02 23:58:09 +0300

    socket io + express

--- HomeWork_6/index.js
+++ HomeWork_6/index.js
@@ -1,23 +1,30 @@
-//const httpServer = require('http').createServer(handler)
-const httpServer = require('http').createServer()
-const io = require('socket.io')(httpServer)
-const fs = require('fs')
+/*
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
 const path = require('path')
 
-/*
-function handler(req, res) {
-  fs.readFile(path.join(__dirname, 'index.html'), 
-  function (err, data) {
-    if (err) {
-      res.writeHead(500)
-      return res.end('Error loading index.html')
-    }
-
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end(data)
-  })
-}
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 */
+
+//app.use(express.static(path.join(__dirname, 'client')))
+
+/*
+io.on('connection', socket => {
+  console.log('New connection...')
+  socket.emit('message', 'Welcome to chat')
+})
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+*/
+
+
+const httpServer = require('http').createServer()
+const io = require('socket.io')(httpServer)
 
 io.on('connection', (socket) => {
   socket.emit('news', { greeting: "Hello socket.io"})
