1aae9430148fca279ef68719411e4034ae2b3ef1
Vadim <zhukov.vd.s@gmail.com>
2021-04-16 00:49:16 +0300

    paradox

---
+++ 3 - Threads/DELETE.js
@@ -0,0 +1,9 @@
+
+let buff = []
+
+function abc (buffer) {
+  buffer[0] = 1
+}
+
+abc(buff)
+console.log(buff);
--- 3 - Threads/script.js
+++ 3 - Threads/script.js
@@ -11,9 +11,15 @@ const writeStream2 = fs.createWriteStream(path.join(__dirname, "/files/2.txt"), 
 const quarterSize = 2
 const halfSize = 4
 
-let chunk = 'abc'
-let buff = []
+let chunk0 = 'abc'
+let buff0 = []
 let isSorted1 = false
+
+function sortChunkOfData (buffer, chunkk) {
+  buffer = buffer.concat(chunkk.split(''))
+  buffer.forEach((element, i, a) => a[i] = Number.parseInt(element))
+  buffer.sort((a, b) => a - b)
+}
 
 readStream0.on('readable', () => { myEmitter.emit('writeSortedDataToFiles') })
 
@@ -22,23 +28,26 @@ myEmitter.on('writeSortedDataToFiles', async () => {
   if (isSorted1) {
     return
   }
-     
 
   // Read and sort the first half of input data
-  chunk = await readStream0.read(halfSize)
-  console.log(chunk);
+  chunk0 = await readStream0.read(halfSize)
+  console.log(chunk0);
   
-  buff = []
-  buff = buff.concat(chunk.split(''))
+  sortChunkOfData(buff0, chunk0)
+
+  /*
+  buff0 = []
+  buff0 = buff.concat(chunk.split(''))
   buff.forEach((element, i, a) => a[i] = Number.parseInt(element))
   buff.sort((a, b) => a - b)
+  */
 
-  for (let i = 0; i < buff.length; i++)
-    writeStream1.write(`${buff[i]}`)
+  for (let i = 0; i < buff0.length; i++)
+    writeStream1.write(`${buff0[i]}`)
 
   // Read and sort the first half of input data
-  chunk = await readStream0.read(halfSize)
-  console.log(chunk);
+  chunk0 = await readStream0.read(halfSize)
+  console.log(chunk0);
   
   buff = []
   buff = buff.concat(chunk.split(''))
@@ -59,16 +68,32 @@ myEmitter.on('readFromTwoFiles', async () => {
   const readStream2 = fs.createReadStream(path.join(__dirname, "/files/2.txt"), {encoding: 'utf-8'})
   const writeStream3 = fs.createWriteStream(path.join(__dirname, "/files/3.txt"), {encoding: 'utf-8'})
 
-
+  let chunk1 = ''
+  let chunk2 = ''
+  let buff1 = []
+  let buff2 = []
+  let isSorted2 = false
 
   readStream1.on('readable', () => {})
   readStream2.on('readable', () => { myEmitter.emit('readPeaceOfData') })
 
   myEmitter.on('readPeaceOfData', async () => { 
-    readStream1.read(quarterSize)
-    readStream2.read(quarterSize)
+    
+    if (isSorted2) 
+      return 
+    
     
 
+    chunk1 = readStream1.read(quarterSize)
+    chunk2 = readStream2.read(quarterSize)
+    console.log(chunk1 + ' ' + chunk2);
+
+    while (chunk1 || chunk2) {
+      sortChunkOfData(buff1, chunk1)
+      sortChunkOfData(buff2, chunk2)
+      console.log(buff1);
+      console.log('AA');
+    }
 
 
   })
