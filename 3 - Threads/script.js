9217730afbbee6f3f6c2f3ab0167d3cad4f2e88e
Vadim <zhukov.vd.s@gmail.com>
2021-04-15 11:13:03 +0300

    halfSize and quarterSize

--- 3 - Threads/script.js
+++ 3 - Threads/script.js
@@ -8,6 +8,9 @@ const readStream0 = fs.createReadStream(path.join(__dirname, "/files/0.txt"), {e
 const writeStream1 = fs.createWriteStream(path.join(__dirname, "/files/1.txt"), {encoding: 'utf-8'})
 const writeStream2 = fs.createWriteStream(path.join(__dirname, "/files/2.txt"), {encoding: 'utf-8'})
 
+const quarterSize = 2
+const halfSize = 4
+
 let chunk = 'abc'
 let buff = []
 let isSorted1 = false
@@ -20,7 +23,7 @@ myEmitter.on('writeSortedDataToFiles', async () => {
     return 
 
   // Read and sort the first half of input data
-  chunk = await readStream0.read(4)
+  chunk = await readStream0.read(halfSize)
   console.log(chunk);
   
   buff = []
@@ -32,7 +35,7 @@ myEmitter.on('writeSortedDataToFiles', async () => {
     writeStream1.write(`${buff[i]}`)
 
   // Read and sort the first half of input data
-  chunk = await readStream0.read(4)
+  chunk = await readStream0.read(halfSize)
   console.log(chunk);
   
   buff = []
@@ -54,14 +57,20 @@ myEmitter.on('writeSortedDataToFiles', async () => {
 myEmitter.on('readFromTwoFiles', async () => {
   const readStream1 = fs.createReadStream(path.join(__dirname, "/files/1.txt"), {encoding: 'utf-8'})
   const readStream2 = fs.createReadStream(path.join(__dirname, "/files/2.txt"), {encoding: 'utf-8'})
+  const writeStream3 = fs.createWriteStream(path.join(__dirname, "/files/3.txt"), {encoding: 'utf-8'})
 
 
 
   readStream1.on('readable', () => {})
   readStream2.on('readable', () => { myEmitter.emit('readPeaceOfData') })
 
-  myEmitter.on('readFromTwoFiles', async () => { 
+  myEmitter.on('readPeaceOfData', async () => { 
+    readStream1.read(quarterSize)
+    readStream2.read(quarterSize)
     
+
+
+
   })
 
 
