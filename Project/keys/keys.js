52d9803162ce28cb1448ab8e4ef8d509b0b8a1e6
Vadim <zhukov.vd.s@gmail.com>
2021-05-14 01:08:10 +0300

    Express

---
+++ Project/keys/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+  private: [
+    '0xda20071a27ced4db86013d2c927c7a864c090dbbf0aa2c0de863b67bf03b6ff6',
+    '0x60ee991e5685d372f2af8fc263f025c2943a4804a345ec11d90ec391bae5aac2',
+    '0xad88357c4e959166d5c924b84d6da01afcfb318cc6158443fe88fd27a3cf1ba5'
+  ],
+  public: [
+    '0xCEC8EF0208152d032d98458b75a9Ae3b13aA603C',
+    '0xB2485E4a4F198a8070c6d34D0518AFEBC2429Eed',
+    '0xeDd3c7d8F00b7320122a37e6dd501456a69B0A53'
+  ]
+}
--- Project/rpc.ts
+++ Project/rpc.ts
@@ -1,31 +1,39 @@
+import keys from './keys/keys'
 import express from 'express';
 import exphbs from 'express-handlebars';
 import path from 'path';
 import Web3 from 'web3';
 
-const app = express()
+console.log(keys);
+
+
+const app = express();
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs', 
   allowProtoMethodsByDefault: true, 
   allowProtoPropertiesByDefault: true
-})
+});
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  //res.send('Hello World!')
+  res.render('index', {
+    title: 'Main page',
+    isHome: true
+  });
 })
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
 
 console.log('CREATED');
 
