ec035c83b3b9ec8b12d47befccba98f07ea945dd
Vadim <zhukov.vd.s@gmail.com>
2021-05-16 23:32:56 +0300

    transactions history

--- Project/keys/users.ts
+++ Project/keys/users.ts
@@ -1,10 +1,11 @@
-export const users = [
+export let users = [
   { 
     email: 'v@mail.ru', 
     password: '123',
     privateKey: '0xc030e372cec640d6cd4490140f793e4351744c8655c79cf03f90258933e00df8',
     publicKey: '0x15cC2f8aB46Ba6FF423D2380e7692f6D548d061d', 
     fullName: 'Vadim Zhukov', 
-    balance: 250
+    balance: 250, 
+    transactions: []
   }
 ]
---
+++ Project/keys/web3.ts
@@ -0,0 +1,4 @@
+//import Web3 from 'web3';
+
+const Web3 = require('web3');
+export const web3 = new Web3("http://127.0.0.1:8545");
