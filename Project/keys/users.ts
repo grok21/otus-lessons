97451e1c91088f01c2209b24a3786434ddecb848
Vadim <zhukov.vd.s@gmail.com>
2021-05-19 23:26:38 +0300

    good wallet

--- Project/keys/users.ts
+++ Project/keys/users.ts
@@ -2,8 +2,8 @@ export let users = [
   { 
     email: 'v@mail.ru', 
     password: '123',
-    privateKey: '0xc030e372cec640d6cd4490140f793e4351744c8655c79cf03f90258933e00df8',
-    publicKey: '0x15cC2f8aB46Ba6FF423D2380e7692f6D548d061d', 
+    privateKey: '0xf8618126133249f678e87abf0d54d833c5553d297a1f781b7251d739c8a9526a',
+    publicKey: '0xB8181734fd47A3964c355FB2C7EF49532A02DBE8', 
     fullName: 'Vadim Zhukov', 
     balance: 250, 
     transactions: []
--- Project/routes/wallet.ts
+++ Project/routes/wallet.ts
@@ -8,15 +8,20 @@ const router = Router();
 
 router.get('/', async (req, res) => {
   try {
-    const candidate = await User.findOne({ email: req.cookies.userInfo.userEmail });
-    console.log(candidate['transactions']);
-    
+    const candidate = await User.findOne({ email: req.cookies.userInfo.userEmail }).lean();
+    let balance = await web3.eth.getBalance(candidate['publicKey']);
+    balance = web3.utils.fromWei(balance);
+    let comission = await web3.eth.getGasPrice();
+    comission = web3.utils.fromWei(comission);
+
     if (candidate) {
       res.render('wallet', {
         title: 'Wallet',
         isWallet: true, 
         isAuthenticated: req.cookies.userInfo.isAuthenticated,
-        transactions: candidate['transactions']
+        balance: balance,
+        transactions: candidate['transactions'],
+        comission: comission
       })
     } else {
       console.log('Sth gone wrong...');
@@ -24,27 +29,7 @@ router.get('/', async (req, res) => {
     }
   } catch (e) {
     console.log(e);
-  }
-  
-  
-  /*
-  const transaction = await web3.eth.getTransaction('0x57662cf4de02cd7efef181c6287b4a6e50c0a2d713e3d8f52a636dd7c44f091c');
-  console.log(transaction);
-  
-  
-  res.render('wallet', {
-    title: 'Wallet',
-    isWallet: true, 
-    isAuthenticated: req.cookies.userInfo.isAuthenticated,
-    transactions: [{
-      hash: '0x57662cf4de02cd7efef181c6287b4a6e50c0a2d713e3d8f52a636dd7c44f091c',
-      from: '0x15cC2f8aB46Ba6FF423D2380e7692f6D548d061d',
-      to: '0xE26E316fD1Af6759BeE245651F4E864fA66d386C',
-      value: 20,
-    }]
-  })
-  */
-    
+  }  
 })
 
 router.post('/', async (req, res) => {
@@ -57,7 +42,7 @@ router.post('/', async (req, res) => {
       web3.eth.sendTransaction({
         from: candidate['publicKey'],
         to: req.body.publicKey,
-        value: req.body.value
+        value: web3.utils.toWei(req.body.value)
       })
       .on('transactionHash', async (hash) => {
         console.log(hash);
